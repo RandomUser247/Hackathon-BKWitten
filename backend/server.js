4b33a54d85b8a3e8f2ffee3b8cdc3447fddd9d98
RandomUser247 <50018058+RandomUser247@users.noreply.github.com>
2023-05-15 19:52:23 +0200

    forgor

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ const db = new sqlite3.Database('/db/test.db')
 
 
 var express = require('express');
-var app = express();app.get('/', function(req, res) {
+
+
+var app = express();
+app.get('/', function(req, res) {
     res.send('API for the backend');
 });
+
+
 
 var server = app.listen(8080, function() {
     console.log("Backend Application listening at http://localhost:8080")
