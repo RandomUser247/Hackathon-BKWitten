2662d5f236f170ea4b7dd65f7954b187e06dc60b
Luca Frala <jdoe@example.com>
2023-05-25 14:20:29 +0200

    Authguard, Loginservice

--- HackathonWebserver/src/app/app.module.ts
+++ HackathonWebserver/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,8 +12,9 @@ import { ProjectOverviewComponent } from "./project-overview/project-overview.co
 import { StartComponent } from "./start/start.component";
 import { RouterModule } from "@angular/router";
 import { ArchiveComponent } from './archive/archive.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthguardGuard } from './shared/authguard.guard';
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,13 +33,15 @@ import { ArchiveComponent } from './archive/archive.component';
     NgbModule,
     AppRoutingModule,
     NgbModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { path: "", component: StartComponent },
-      { path: "start", component: StartComponent },
-      { path: "login", component: LoginComponent },
-      { path: "project", component: ProjectComponent },
-      { path: "overview", component: ProjectOverviewComponent},
-      { path: "archive", component: ArchiveComponent}
+      { path: "", component: StartComponent, canActivate: [AuthguardGuard] },
+      { path: "start", component: StartComponent, canActivate: [AuthguardGuard] },
+      { path: "login", component: LoginComponent, canActivate: [AuthguardGuard] },
+      { path: "project", component: ProjectComponent, canActivate: [AuthguardGuard] },
+      { path: "overview", component: ProjectOverviewComponent, canActivate: [AuthguardGuard] },
+      { path: "archive", component: ArchiveComponent, canActivate: [AuthguardGuard] }
     ]),
   ],
   providers: [],
