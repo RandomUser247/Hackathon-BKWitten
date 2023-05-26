82a75d3850f302254addabaf3b01d774da6111a0
Luca Frala <jdoe@example.com>
2023-05-26 07:59:00 +0200

    Projectedit

--- HackathonWebserver/src/app/app.module.ts
+++ HackathonWebserver/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { AuthguardGuard } from './shared/authguard.guard';
       { path: "start", component: StartComponent, canActivate: [AuthguardGuard] },
       { path: "login", component: LoginComponent, canActivate: [AuthguardGuard] },
       { path: "project", component: ProjectComponent, canActivate: [AuthguardGuard] },
+      { path: "edit", component: ProjectEditComponent, canActivate: [AuthguardGuard] },
       { path: "overview", component: ProjectOverviewComponent, canActivate: [AuthguardGuard] },
       { path: "archive", component: ArchiveComponent, canActivate: [AuthguardGuard] }
     ]),
---
+++ HackathonWebserver/src/app/interfaces/project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+    id: number,
+    projecttitle: string,
+    projectdecription: string,
+    projectimage: string,
+    projectvideo: string,
+}
