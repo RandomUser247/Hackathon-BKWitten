63f03264463d53509b7450879f01bf111bf14866
bieskian <kbiesemann@doerken.de>
2023-05-26 09:32:44 +0200

    .

--- HackathonWebserver/src/app/archive/archive.component.ts
+++ HackathonWebserver/src/app/archive/archive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-archive',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./archive.component.css']
 })
 export class ArchiveComponent {
+
+  STREAMS = [
+
+    {id: 1, url: ''},
+    {id: 2, url: 'https://www.youtube.com/embed/dQw4w9WgXcQ'},
+    {id: 5, url: 'https://www.youtube.com/embed/dQw4w9WgXcQ'},
+    {id: 3, url: 'https://www.youtube.com/embed/dQw4w9WgXcQ'},
+    {id: 4, url: 'https://www.youtube.com/embed/dQw4w9WgXcQ'}
+
+  ]
 
 }
