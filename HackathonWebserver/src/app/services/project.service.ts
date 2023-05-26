9be680362504871163cd861e81e48f969b8be780
klimakleber <132546541+klimakleber@users.noreply.github.com>
2023-05-26 08:38:29 +0200

    Project Edit
    Project Edit

---
+++ HackathonWebserver/src/app/services/project.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Projects } from '../interfaces/project';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  constructor(private http: HttpClient) { }
+  projectList: Projects[];
+
+  //Dieses NgOnInit muss später in die overview Komponente, damit diese beim Start der Komponente geladen werden
+
+  //Alle Projekte aus der Datenbank lesen
+  readProjectsFromDatabase(): Observable<Projects> {
+    return this.http.get<Projects>('http://localhost:8080/projects');
+  }
+  //Ein Projekt aus der Datenbank per ID lesen
+  readSingleProjectFromDatabase(projectid: number): Observable<Projects> {
+    return this.http.get<Projects>('http://localhost:8080/projects/' + projectid);
+  }
+  //Ein bestehendes Projekt updaten oder ein neues hinzufügen
+  updateOrAddProject(project: Projects): Observable<Projects> {
+    return this.http.post<Projects>('http://localhost:8080/projects', project);
+  }
+  //Ein Projekt löschen
+  deleteProject(projectID: number): Observable<Projects> {
+    return this.http.delete<Projects>('http://localhost:8080/projects/' + projectID);
+  }
+  filterProjectsByName(searchText: string) {
+    this.projectList.filter((s) => {
+      return s.projecttitle.toLowerCase().includes(searchText.toLowerCase());
+    });
+  }
+}
